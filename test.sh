a9993202c63f75b5b4ec26784ab02be6d3e3916a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-27 12:44:37 -0300

    fix and implement the first automatic test

---
+++ test.sh
@@ -0,0 +1,23 @@
+#! bash
+# 
+# ========== Running the default test ========== #
+
+# copying unitt to tests
+
+cp unitt.art tests/testDefault
+cd tests/testDefault
+
+# running
+
+./tester.art > output
+
+# checking the return
+
+cmp -- ./sample ./output    \
+    && echo sucess          \
+    || echo failed
+
+# cleaning test
+rm unitt.art
+rm output
+
