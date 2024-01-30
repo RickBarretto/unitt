717928107f25b28c35458a74969c92042ff41099
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-30 19:22:54 -0300

    [test]: add test files

--- test.sh
+++ test.sh
@@ -24,3 +24,28 @@ cmp -- sample output && echo "Sucess!"   \
 rm unitt.art
 rm output
 
+
+# ========== Running the finder test ========== #
+
+# copying unitt to tests
+
+cp unitt.art tests/test-finder
+cd tests/test-finder
+
+# running
+
+./tester.art > output
+
+# checking the return
+
+cmp -- sample output && echo "Sucess!"   \
+    || (
+        echo "Failed!" &&
+        echo "" && 
+        diff sample output
+    ) 
+
+# cleaning test
+rm unitt.art
+rm output
+
