0fed91d410e50c3ec48eb7219cffabc026279154
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-06 21:32:02 -0300

    Format file

--- unitt/src/main.rs
+++ unitt/src/main.rs
@@ -4,20 +4,16 @@ mod display;
 mod models;
 mod runner;
 
-use std::{env, path::{Path, PathBuf}};
-
 use clap::Parser;
 
 use models::config::Config;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = env::set_current_dir(Path::new(".."))?;
-
     let args = cli::Arguments::parse();
     let config: Config = cli::actual_config(&args)?;
-
     let arturo = config.arturo.clone();
+
     runner::reset_cache(config.cache.clone());
     runner::generate_tests(&config, &arturo).await;
 
